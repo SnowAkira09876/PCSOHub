9b4838030b6982b446b4e864edc38e980e4342dd
Akira Snow <snowakira2814@gmail.com>
2023-05-13 20:00:55 +0800

    Use Scroll away search bar

--- app/src/main/java/com/ph/pcsolottowatcher/recyclerviews/adapter/allresults/AllResultsAdapter.java
+++ app/src/main/java/com/ph/pcsolottowatcher/recyclerviews/adapter/allresults/AllResultsAdapter.java
@@ -6,10 +6,14 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView.ViewHolder;
 import com.ph.pcsolottowatcher.common.recyclerview.BaseListAdapter;
 import com.ph.pcsolottowatcher.databinding.AllHistoryItemBinding;
+import com.ph.pcsolottowatcher.databinding.AllHistoryLatestItemBinding;
 import com.ph.pcsolottowatcher.pojos.results.local.LocalHistoryModel;
+import com.ph.pcsolottowatcher.recyclerviews.holder.allresults.AllHistoryLatestViewHolder;
 import com.ph.pcsolottowatcher.recyclerviews.holder.allresults.AllHistoryViewHolder;
 
 public class AllResultsAdapter extends BaseListAdapter<LocalHistoryModel> {
+  private static final int ALL_HISTORY_LATEST_ITEM = 0;
+  private static final int ALL_HISTORY_ITEM = 1;
 
   public AllResultsAdapter(DiffUtil.ItemCallback<LocalHistoryModel> diff) {
     super(diff);
@@ -17,15 +21,40 @@ public class AllResultsAdapter extends BaseListAdapter<LocalHistoryModel> {
 
   @Override
   public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-    return new AllHistoryViewHolder(
-        AllHistoryItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    switch (viewType) {
+      case 0:
+        return new AllHistoryLatestViewHolder(
+            AllHistoryLatestItemBinding.inflate(
+                LayoutInflater.from(parent.getContext()), parent, false));
+      case 1:
+        return new AllHistoryViewHolder(
+            AllHistoryItemBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+    }
+
+    return null;
   }
 
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
-    AllHistoryViewHolder viewHolder = (AllHistoryViewHolder) holder;
-    LocalHistoryModel model = getItem(position);
+    switch (getItemViewType(position)) {
+      case ALL_HISTORY_LATEST_ITEM:
+        AllHistoryLatestViewHolder viewHolder = (AllHistoryLatestViewHolder) holder;
+        LocalHistoryModel model = getItem(position);
 
-    viewHolder.bind(model);
+        viewHolder.bind(model);
+        break;
+      case ALL_HISTORY_ITEM:
+        AllHistoryViewHolder viewHolder1 = (AllHistoryViewHolder) holder;
+        LocalHistoryModel model1 = getItem(position);
+
+        viewHolder1.bind(model1);
+        break;
+    }
+  }
+
+  @Override
+  public int getItemViewType(int position) {
+    if (position == 0) return ALL_HISTORY_LATEST_ITEM;
+    else return ALL_HISTORY_ITEM;
   }
 }
